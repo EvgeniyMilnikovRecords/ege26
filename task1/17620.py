36c907f29a8e7cb7a5da9ff6f88737ed1f9101cb
henriquexap <zhopoprod@gmail.com>
2025-12-18 19:40:24 +0200

    самостоятельная задания 18.12.25

---
+++ task1/17620.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+
+graph = 'ab bd dg gc ce ef af fb ed'.split()
+matrix = '256 134 267 27 16 135 34'.split()
+print(*range(1, 8))
+for i in permutations('abcdefg'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x , y in graph):
+        print(*i)
---
+++ task1/23185.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+
+graph = 'ah hf fe ed dg ga gc cb bh bf'.split()
+matrix = '478 38 256 15 34 37 168 127'.split()
+print(*range(1, 9))
+for i in permutations('abcdefgh'):
+        if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+            print(*i)
