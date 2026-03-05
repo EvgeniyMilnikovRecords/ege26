f4fc5cfcf192cb8040e7594ef6cb339a01920c41
henriquexap <zhopoprod@gmail.com>
2026-03-05 19:44:35 +0200

    нет вопросов

---
+++ task5/27617.py
@@ -0,0 +1,10 @@
+
+for n in range(1, 100_000):
+    r = f'{n:b}'
+    if n % 3 == 0:
+        r += r[-3:]
+    else:
+        r += f'{(n % 3) * 3:b}'
+    r = int(r, 2)
+    if r < 130:
+        print(max(a))
